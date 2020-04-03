e77a6e6678b34d2d0580f218d82f90387f030934
Ihor Ivliev <igor852745@gmail.com>
2020-04-03 19:09:34 +0300

    Create Clock

---
+++ src/conponents/Clock/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from './styles.module.css'
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(
+            ()=> this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div className={styles.time}>
+                <h2 >{this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+export default Clock;
